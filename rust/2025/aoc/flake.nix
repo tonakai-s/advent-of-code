8281f5814f9bcada0fd33b022961df9188080632
tonakai-s <rdorneles64@gmail.com>
2025-12-01 21:12:22 -0300

    restructure tree

---
+++ rust/2025/aoc/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, self }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      packages."x86_64-linux".default = pkgs.rustPlatform.buildRustPackages {
+        name = "aoc";
+        src = ./.;
+        buildInputs = [ pkgs.glib ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          clippy
+          rust-analyzer
+        ];
+        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+    };
+}
