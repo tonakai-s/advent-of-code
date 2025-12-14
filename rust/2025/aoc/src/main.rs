aa85f2bd41d1654acbbedcaacdaf11fe8b2390d5
tonakai-s <rdorneles64@gmail.com>
2025-12-14 18:33:18 -0300

    finish day 2

--- rust/2025/aoc/src/main.rs
+++ rust/2025/aoc/src/main.rs
@@ -3,4 +3,5 @@ use aoc::problems;
 fn main() {
     problems::problem1::part1();
     problems::problem2::part1();
+    problems::problem2::part2();
 }
--- rust/2025/aoc/src/problems/problem2.rs
+++ rust/2025/aoc/src/problems/problem2.rs
@@ -1,13 +1,11 @@
 use crate::common;
 
 pub fn part1() {
-    // let input = common::read_ex_inp(2);
-    let input = common::read_inp(2);
+    let input = common::read_ex_inp(2);
 
     let mut res: usize = 0;
     input.split(",").for_each(|r| {
         let (start, end) = r.split_once("-").unwrap();
-        println!("{start} - {end}");
         for n in start.trim().parse::<usize>().unwrap()..=end.trim().parse::<usize>().unwrap() {
             let nstr = n.to_string();
             if nstr.len() % 2 == 1 {
@@ -20,5 +18,39 @@ pub fn part1() {
         }
     });
     
-    println!("Part 1: {res}");
+    println!("2/1: {res}");
+}
+
+pub fn part2() {
+    let input = common::read_inp(2);
+
+    let mut res: usize = 0;
+    input.split(",").for_each(|r| {
+        let (start, end) = r.split_once("-").unwrap();
+        for n in start.parse::<usize>().unwrap()..=end.trim().parse::<usize>().unwrap() {
+            let nstr = n.to_string();
+
+            if nstr.len() < 2 {
+                continue;
+            }
+            if {
+                let fc = nstr.chars().next().unwrap();
+                nstr.chars().all(|c| c == fc)
+            } {
+                res += n;
+                continue;
+            }
+            // TODO: must have a more performatic way to check this
+            for chunk_size in 2..=(nstr.len()/2) {
+                let chunk = nstr.get(0..chunk_size).unwrap().to_string();
+                if chunk.repeat(nstr.len()/chunk_size) != nstr {
+                    continue;
+                }
+                res += n;
+                break;
+            }
+        }
+    });
+    
+    println!("2/2: {res}");
 }
